eef5986f90f876c1ed077491df5033201bfb7bd3
MikhailButyanov <mikhail@butyanov.ru>
2023-03-14 13:41:20 +0300

    add Ex005

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:  ");
+string username = Console.ReadLine();
+if(username.ToLower() == "vova")
+{
+    Console.WriteLine("Привет, Вовка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
+
